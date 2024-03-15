69845d47691c807a0f4d9e503b001c9e9062bf9c
benazevedo <ben_azevedo@outlook.com>
2024-03-15 09:17:04 -0700

    Cleaned up code and fixed resizing issues

--- App.tsx
+++ App.tsx
@@ -10,7 +10,28 @@ const App: React.FC = () => {
   const [lineLength, setLineLength] = useState<string>('');
   const [feet, setFeet] = useState<boolean>(true);
 
-  const table12 = [
+  const sliders = [
+    {
+      label: 'Speed',
+      unit: 'mph',
+      min: 1,
+      max: 2,
+      step: 0.1,
+      value: speed,
+      setValue: setSpeed,
+    },
+    {
+      label: 'Weight',
+      unit: 'oz',
+      min: 1,
+      max: 5,
+      step: 0.5,
+      value: weight,
+      setValue: setWeight,
+    },
+  ];
+
+  const table12: number[][] = [
     [2, 2, 4, 6, 7, 13, 15, 16, 18, 20, 22, 24, 25, 23, 24],
     [4, 5.5, 8.5, 10, 11, 16, 19, 20, 22, 23.5, 25.5, 28, 29, 27.5, 29],
     [6, 9, 13, 14, 15, 19, 23, 24, 26, 27, 29, 32, 33, 32, 34],
@@ -25,7 +46,7 @@ const App: React.FC = () => {
     [10, 20, 28, 28, 35, 42, 45, 49, 52, 57, 61, 65, 67, 73, 73],
   ];
 
-  const table15 = [
+  const table15: number[][] = [
     [1, 4, 8, 8, 9, 11, 12, 13, 16, 18, 20, 17, 15, 21, 23],
     [2, 6, 8.5, 9.5, 11, 14, 16, 17, 20, 22.5, 23.5, 22.5, 20.5, 25.5, 27.5],
     [3, 8, 9, 11, 13, 17, 20, 21, 24, 27, 27, 28, 26, 30, 32],
@@ -40,79 +61,50 @@ const App: React.FC = () => {
     [5, 13, 17, 22, 23, 30, 31, 39, 41, 47, 50, 51, 47, 51, 50],
   ];
 
-  function interpolateSpeed(speed: number) {
-    const table = [];
+  const lineLengths: number[] = [
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150,
+  ];
 
-    for (let i = 0; i < table12.length; i++) {
-      const row = [];
-      for (let j = 0; j < table12[i].length; j++) {
-        const value12 = table12[i][j];
+  function interpolateSpeed(speed: number): number[][] {
+    return table12.map((row, i) =>
+      row.map((value12, j) => {
         const value15 = table15[i][j];
         const interval = Math.abs(value12 - value15) / 3;
-        let interpolatedValue;
-
-        if (speed < 1.2) {
-          interpolatedValue =
-            value12 +
-            (speed - 1.2) * 10 * interval * (value12 > value15 ? -1 : 1);
-        } else if (speed > 1.5) {
-          interpolatedValue =
-            value15 +
-            (speed - 1.5) * 10 * interval * (value15 < value12 ? 1 : -1);
-        } else {
-          // 1.2 <= speed <= 1.5
-          interpolatedValue =
-            value12 + ((speed - 1.2) / 0.3) * (value15 - value12);
-        }
-
-        row.push(Math.max(0, interpolatedValue)); // Ensure non-negative values
-      }
-      table.push(row);
-    }
-
-    return table;
+        const interpolatedValue =
+          speed < 1.2
+            ? value12 +
+              (speed - 1.2) * 10 * interval * Math.sign(value15 - value12)
+            : speed > 1.5
+            ? value15 +
+              (speed - 1.5) * 10 * interval * Math.sign(value12 - value15)
+            : value12 + ((speed - 1.2) / 0.3) * (value15 - value12);
+        return Math.max(0, interpolatedValue);
+      }),
+    );
   }
 
-  function customRound(num: number) {
-    // First, round the number to the desired precision
+  function customRound(num: number): number {
     const roundedNum = Math.round(num * 100) / 100;
-
-    // Check if the rounded number is an integer
-    if (Number.isInteger(roundedNum)) {
-      // If it's an integer, return it as is
-      return roundedNum;
-    } else {
-      // If it's not an integer, ensure it retains the necessary decimal places
-      // Convert to string to check for trailing zeros
-      const strNum = roundedNum.toString();
-
-      // If there's a decimal point, and the only digit(s) after it are '0', parse as integer
-      if (strNum.includes('.') && /^0+$/.test(strNum.split('.')[1])) {
-        return parseInt(strNum, 10);
-      }
-
-      // Otherwise, return the number with its significant decimal digits
-      return roundedNum;
-    }
+    return Number.isInteger(roundedNum) ||
+      /^0+$/.test(roundedNum.toString().split('.')[1])
+      ? parseInt(roundedNum.toString(), 10)
+      : roundedNum;
   }
 
-  function interpolateLineLength(depths: number[], targetDepth: number) {
-    const lineLengths = [
-      10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150,
-    ];
-    setFeet(false);
+  function interpolateLineLength(
+    depths: number[],
+    targetDepth: number,
+  ): number | string {
     if (targetDepth < depths[0]) return 'Take some weight off amigo!';
     for (let i = 0; i < depths.length; i++) {
       if (depths[i] > targetDepth) {
-        if (i === 0) {
-          return 10 * (targetDepth / depths[0]);
-        } else {
-          let max = depths[i] - depths[i - 1];
-          let range = targetDepth - depths[i - 1];
-          setFeet(true);
-          let rounded = customRound(lineLengths[i - 1] + 10 * (range / max));
-          return rounded;
-        }
+        return i === 0
+          ? 10 * (targetDepth / depths[0])
+          : customRound(
+              lineLengths[i - 1] +
+                10 *
+                  ((targetDepth - depths[i - 1]) / (depths[i] - depths[i - 1])),
+            );
       }
     }
     return "You're gonna need to add some weight boss";
@@ -122,85 +114,67 @@ const App: React.FC = () => {
     speed: number,
     weight: number,
     targetDepth: number,
-  ) {
-    let table;
-    if (speed === 1.2) {
-      table = table12;
-    } else if (speed === 1.5) {
-      table = table15;
-    } else {
-      table = interpolateSpeed(speed);
-    }
-
-    // pick correct row for weight
-    const weightIndex = (weight - 1) / 0.5;
+  ): number | string {
+    const table =
+      speed === 1.2
+        ? table12
+        : speed === 1.5
+        ? table15
+        : interpolateSpeed(speed);
+    const weightIndex = Math.round((weight - 1) / 0.5);
     const depths = table[weightIndex];
-
-    // interpolate linelength value for targetDepth
-    const lineLength = interpolateLineLength(depths, targetDepth);
-    return lineLength;
+    return interpolateLineLength(depths, targetDepth);
   }
 
   useEffect(() => {
     const result = interpolationManager(speed, weight, targetDepth);
     setLineLength(String(result));
-  }, [speed, weight, targetDepth]); // This effect depends on speed, weight, and targetDepth
+  }, [speed, weight, targetDepth]);
 
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#F0F8FF'}}>
-      <View style={styles.container}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginBottom: 30,
-          }}>
-          <Image source={require('./assets/mike.png')} style={styles.image} />
-          <View
-            style={{
-              flexDirection: 'column',
-              justifyContent: 'center',
-              height: 200,
-            }}>
-            <Text style={styles.title}>Little Mike's</Text>
-            <Text style={styles.title}>Trolling</Text>
-            <Text style={styles.title}>Calculator</Text>
+    <SafeAreaView style={styles.screen}>
+      <View style={styles.headerContainer}>
+        <Image source={require('./assets/mike.png')} style={styles.image} />
+        <View style={styles.titlesContainer}>
+          <Text style={styles.title}>Little Mike's</Text>
+          <Text style={styles.title}>Trolling</Text>
+          <Text style={styles.title}>Calculator</Text>
+        </View>
+      </View>
+
+      <View style={styles.inputContainer}>
+        {sliders.map((slider, index) => (
+          <View key={index} style={styles.sliderContainer}>
+            <Text style={styles.subtitle}>
+              {slider.label}:{' '}
+              <Text style={styles.boldText}>
+                {slider.value.toFixed(1)} {slider.unit}
+              </Text>
+            </Text>
+            <Slider
+              style={styles.slider}
+              minimumValue={slider.min}
+              maximumValue={slider.max}
+              step={slider.step}
+              value={slider.value}
+              onValueChange={slider.setValue}
+            />
           </View>
-        </View>
-
-        <View style={styles.sliderContainer}>
-          <Text style={styles.subtitle}>Speed: {speed.toFixed(1)} mph</Text>
-          <Slider
-            style={styles.slider}
-            minimumValue={1}
-            maximumValue={2}
-            step={0.1}
-            value={speed}
-            onValueChange={value => setSpeed(value)}
-          />
-        </View>
-
-        <View style={styles.sliderContainer}>
-          <Text style={styles.subtitle}>Weight: {weight.toFixed(1)} oz</Text>
-          <Slider
-            style={styles.slider}
-            minimumValue={1}
-            maximumValue={5}
-            step={0.5}
-            value={weight}
-            onValueChange={value => setWeight(value)}
-          />
-        </View>
-
-        <Text style={styles.subtitle}>Target Depth: {targetDepth} ft</Text>
+        ))}
+        <Text style={styles.subtitle}>
+          Target Depth: <Text style={styles.boldText}>{targetDepth} ft</Text>
+        </Text>
         <ReelDial
           min={0}
           max={100}
           step={1}
           onValueChange={(value: number) => setTargetDepth(value)}
         />
-        <View style={styles.outputContainer}>
-          <Text style={styles.output}>
+      </View>
+
+      <View style={styles.outputContainer}>
+        <View style={styles.output}>
+          <Text style={styles.outputText}>
             {feet && 'Let out '}
             {lineLength} {feet && 'feet of line'}
           </Text>
@@ -211,21 +185,53 @@ const App: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  boldText: {
+    fontWeight: 'bold',
+  },
+  headerContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#F0F8FF', // Soft background color
+    justifyContent: 'center',
+    height: '30%',
+  },
+  inputContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '55%',
   },
   image: {
-    width: 140,
-    height: 200,
+    width: 112,
+    height: 160,
     resizeMode: 'contain',
     marginBottom: 20, // Add some space below the image
   },
+  output: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#B0C4DE', // Light theme color for the output container
+    borderWidth: 1,
+    borderColor: '#708090', // Border color that matches the theme
+  },
+  outputContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '15%',
+  },
+  outputText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#000000', // Dark color for text
+  },
+  screen: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 20,
+    backgroundColor: '#F0F8FF', // Soft background color
+  },
   sliderContainer: {
     width: '90%',
-    marginBottom: 30,
+    marginBottom: '3%',
   },
   slider: {
     width: '100%',
@@ -237,24 +243,16 @@ const styles = StyleSheet.create({
     marginBottom: 10, // Space between text and slider
   },
   title: {
-    fontSize: 32,
+    fontSize: 30,
     fontWeight: 'bold',
     color: '#2F4F4F', // Theme color for titles
     textAlign: 'center', // Center-align titles
     marginBottom: 10, // Space below the title
   },
-  outputContainer: {
-    marginTop: 30,
-    padding: 15,
-    borderRadius: 10,
-    backgroundColor: '#B0C4DE', // Light theme color for the output container
-    borderWidth: 1,
-    borderColor: '#708090', // Border color that matches the theme
-  },
-  output: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    color: '#000000', // Dark color for text
+  titlesContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: 200,
   },
 });
 
